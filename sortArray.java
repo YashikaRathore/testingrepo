f58dfd2e37f0a0fd486201f1981b32b6f286f5eb
YashikaRathore <45383165+YashikaRathore@users.noreply.github.com>
2018-11-29 16:28:03 +0530

    'secondCommit'

---
+++ sortArray.java
@@ -0,0 +1,18 @@
+public class SortArray{
+
+	public static void main(String args[]){
+		int inputArray[] = {1,2,56,89,45,66,2,34,72,12};
+		int outputArray[] = {};
+		for(i=0;i<=inputArray.length;i++){
+			int x = inputArray[i];
+			if(x/2)
+
+		}
+
+
+
+	}
+
+
+
+}
